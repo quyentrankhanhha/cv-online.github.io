c8a1484d496572f525cbed1455bf1fc3cebc9de7
quyentrankhanhha <ha.quyen@thuocsi.vn>
2021-10-04 04:03:43 +0700

    done UI

--- js/main.js
+++ js/main.js
@@ -29,15 +29,42 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET  ====================*/
+function scaleCv() {
+  document.body.classList.add('scale-cv')
+}
 
-/*==================== QUALIFICATION TABS ====================*/
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADING ====================*/
+function removeScale() {
+  document.body.classList.remove('scale-cv')
+}
 
-/*==================== SERVICES MODAL ====================*/
+/*==================== GENERATE PDF ====================*/
+// PDF generated area
+let areaCv = document.getElementById('area-cv')
+let resumeButton = document.getElementById('resume-button')
+
+// html2pdf options
+let opt = {
+  margin: 0,
+  filename: 'HaQuyenResume.pdf',
+  image: { type: 'jpeg', quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: 'a4', orientation: 'portrait' },
+}
+
+// function to call areaCv and html2pdf
+function generateResume() {
+  html2pdf(areaCv, opt)
+}
+
+resumeButton.addEventListener('click', () => {
+  scaleCv()
+  generateResume()
+  setTimeout(removeScale, 5000)
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
-/*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
@@ -66,5 +93,45 @@ window.addEventListener('scroll', scrollActive)
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top')
+  if (this.scrollY >= 200) scrollTop.classList.add('show-scroll')
+  else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/
+localStorage.setItem('selected-theme', 'light')
+localStorage.setItem('selected-icon', 'moon')
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// previously selected topic
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  )
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](
+    iconTheme
+  )
+
+  // active/ deactive the theme manually with the button
+  themeButton.addEventListener('click', () => {
+    // add or remove the dark/ icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // save the theme and current icon
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+  })
+}
